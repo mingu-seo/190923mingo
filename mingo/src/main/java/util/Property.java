e871c7e4434f83e5cdd276bd8749878a0e082aca
HSK <HSK@DESKTOP-3SENLSR>
2020-01-14 20:17:08 +0900

    상세페이지 다중 이미지 업로드 기능 추가 및 수정/최재명/2020-01-14 20:17

---
+++ mingo/src/main/java/util/Property.java
@@ -0,0 +1,6 @@
+package util;
+
+public class Property {
+
+	public static final String cafe_img_path = "C:/Users/HSK/git/190923mingo/190923mingo/mingo/src/main/webapp/upload";
+}
---
+++ mingo/src/main/java/vo/CafeImageVO.java
@@ -0,0 +1,28 @@
+package vo;
+
+public class CafeImageVO {
+	private int cafe_image_id;
+	private int cafe_id;
+	private String url;
+	public int getCafe_image_id() {
+		return cafe_image_id;
+	}
+	public void setCafe_image_id(int cafe_image_id) {
+		this.cafe_image_id = cafe_image_id;
+	}
+	public int getCafe_id() {
+		return cafe_id;
+	}
+	public void setCafe_id(int cafe_id) {
+		this.cafe_id = cafe_id;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	
+	
+}
